308ffea68c2cc02496e8d6f3442f424371dd7a54
Ashir-Bhatti <ashirbhatti98@gmail.com>
2022-05-13 17:43:36 +0500

    Little Update in code

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -8,19 +8,29 @@
                 <div class="card-header">{{ __('Register') }}</div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('user.store') }}">
+                        @if ( Session::get('success'))
+                        <div class="alert alert-success">
+                            {{ Session::get('success') }}
+                        </div>
+                        @endif
+                        @if ( Session::get('error'))
+                        <div class="alert alert-danger">
+                            {{ Session::get('error') }}
+                        </div>
+                        @endif
                         @csrf
                         
                         <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
                             <div class="col-md-6">
-                             <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
                             </div>
                         </div>
                         
                         <div class="row mb-3">
-                           <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-                             <div class="col-md-6">
-                              <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
                             </div>
                         </div>
                         
@@ -30,20 +40,20 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">   
                             </div>
                         </div>
+                    </div>
+                    
+                    <div class="row mb-3">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Add New') }}
+                            </button>
                         </div>
-                        
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Add New') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 </div>
 
 @endsection
